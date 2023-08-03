14dfad280ba7132d7a3c0cb308632d1a6d6e6de3
M-- <masoud.m.soltani@gmail.com>
2023-08-03 10:51:33 -0400

    Create List_of_Comments.js

---
+++ List_of_Comments.js
@@ -0,0 +1,60 @@
+callback(
+[
+{ "name": "[Q] Elaborate On What Expected", "description": "Please [edit] your question to provide more details on what you expected to happen versus what actually happened. This will help readers figure out what didn't work and how to answer your question accordingly."},
+
+{ "name": "[Q] Be Specific About Problem", "description": "Please [edit] your question to specifically and clearly define the problem that you are trying to solve. Additional details — including all relevant code, error messages, and debugging logs — will help readers to better understand your problem and [what you are asking]."},
+
+{ "name": "[Q] Provide Background Research", "description": "Please [edit] your question to provide more background details on your problem. Telling readers about your research, what you have already tried, and why it didn't meet your needs will allow them to understand it better."},
+
+{ "name": "[Q] Include MRE", "description": "Please [edit] your question to include a [mre] so that readers can run your code to answer your question."},
+
+{ "name": "[Q] R specific MRE", "description": "You should provide a [minimal reproducible example of your code and data along with relevant errors](https://stackoverflow.com/questions/5963269/how-to-make-a-great-r-reproducible-example). Please [edit] your question to include a [mre] so that readers can run your code to answer your question."},
+
+{ "name": "[Q] Convert Images", "description": "Please [edit] your question to convert images into text, and use a [code block] for any code you have. Readers can then search the text or run the code to reproduce your issue."},
+
+{ "name": "[Q] Non-English Content", "description": "Stack Overflow is an [English-only site](//meta.stackoverflow.com/a/297680). Please try your best to [edit] your question to rewrite it in English."},
+
+{ "name": "[Q] Revise Title", "description": "Please [edit] your question to convert images into text, and use a [code block] for any code you have. Readers can then search the text or run the code to reproduce your issue."},
+
+{ "name": "[Q] Improve Code Formatting", "description": "The lack of proper code formatting makes it difficult for readers to help you. Please [edit] your question to format inline code properly and to use a [code block] for any code or error messages you have included."},
+
+{ "name": "[A] NAA-Comment", "description": "This does not provide an answer to the question. Once you have sufficient [reputation](${reputationHelp}) you will be able to [comment on any post](${commentHelp}); instead, [provide answers that don't require clarification from the asker](//meta.stackexchange.com/q/214173)."},
+
+{ "name": "[A] NAA-Library", "description": "Please don't just post some tool or library as an answer. At least demonstrate [how it solves the problem](//meta.stackoverflow.com/a/251605) in the answer itself."},
+
+{ "name": "[A] NAA-Asking Questions", "description": "This does not provide an answer to the question. You can [search for similar questions](/search), or refer to the related and linked questions on the right-hand side of the page to find an answer. If you have a related but different question, [ask a new question](/questions/ask), and include a link to this one to help provide context. See: [Ask questions, get answers, no distractions](/tour)"},
+
+{ "name": "[A] NAA-Generic", "description": "This post doesn't look like an attempt to answer this question. Every post here is expected to be an explicit attempt to *answer* this question; if you have a critique or need a clarification of the question or another answer, you can [post a comment](${commentHelp}) directly below the post. Please remove this answer and create either a comment or a new question. See: [Ask questions, get answers, no distractions](/tour)."},
+
+{ "name": "[A] Duplicate Answer", "description": "Please don't add the [same answer to multiple questions](//meta.stackexchange.com/q/104227). If it is not a duplicate, [edit] the answer and tailor the post to the question."},
+
+{ "name": "[A] Link to another answer on SO", "description": "Instead of posting an answer which merely links to another answer, please instead [flag the question](${flagPosts}) as a duplicate."},
+
+{ "name": "[Q] More than one question asked", "description": "It is preferred if you can post separate questions instead of combining your questions into one. That way, it helps the people answering your question and also others hunting for at least one of your questions. Thanks!"},
+
+{ "name": "[Q] Duplicate Closure", "description": "This question will probably be closed as a duplicate soon. If the answers from the duplicates don't fully address your question please edit it to include why and flag this for re-opening. Thanks!"},
+
+{ "name": "[A] Answers just to say Thanks!", "description": "Please don't add \"thanks\" as answers. Invest some time in the site and you will gain sufficient <a href=\"//$SITEURL$/privileges\">privileges</a> to upvote answers you like, which is [the $SITENAME$ way of saying thank you](https://$SITEURL$/help/someone-answers)."},
+
+{ "name": "[A] OP saying thanks, and just thanks, in the comments", "description": "If you have sufficient <a href=\"//$SITEURL$/privileges\">privileges</a>, you should consider ▲upvoting answer(s) you like, which is [the $SITENAME$ way of saying thank you](https://$SITEURL$/help/someone-answers). You can also accept the answer that solved your issue or helped you the most by clicking on the ✔️checkmakr on the left side of the post."},
+
+{ "name": "[Q] OP saying thanks, and just thanks, in the comments", "description": "If you have sufficient <a href=\"//$SITEURL$/privileges\">privileges</a>, you should consider ▲upvoting answer(s) you like, which is [the $SITENAME$ way of saying thank you](https://$SITEURL$/help/someone-answers). You can also accept the answer that solved your issue or helped you the most by clicking on the ✔️checkmakr on the left side of the post."},
+
+{ "name": "[A] Nothing but a URL (and isn't spam)", "description": "A link to a solution is welcome, but please ensure your answer is useful without it; [add context around the link](//meta.stackexchange.com/a/8259), include the essential parts of the answer here, and provide the link for reference."},
+
+{ "name": "[A] Requests to OP for further information", "description": "This is really a comment, not an answer. With a bit more rep, <a href=\"//$SITEURL$/privileges/comment\">you will be able to post comments</a>. For the moment I've added the comment for you, and I'm flagging this post for deletion."},
+
+{ "name": "[A] Should be an edit", "description": "Please use the <em>Post answer</em> button only for actual answers. You should modify the original post to add additional information."},
+
+{ "name": "[A] OP adding a new question as an answer", "description": "If you have another question, please ask it by clicking the <a href=\"//$SITEURL$/questions/ask\">Ask Question</a> button."},
+
+{ "name": "[A] Another user adding a "Me too!"", "description": "If you have a NEW question, please ask it by clicking the <a href=\"//$SITEURL$/questions/ask\">Ask Question</a> button. If you have sufficient reputation, <a href=\"//$SITEURL$/privileges/vote-up\">you may upvote</a> the question. Alternatively, \"star\" it as a favorite and you will be notified of any new answers."},
+
+{ "name": "[EQ] Improper tagging", "description": "The tags you were using are not appropriate for this question. Please review <a href=\"//$SITEURL$/help/tagging\">What are tags, and how should I use them?</a>"},
+
+{ "name": "[Q] Vandalism", "description": "Please do not vandalize your posts. By posting on the Stack Exchange network, you've granted a non-revocable right for SE to distribute that content (under the [CC BY-SA 4.0 license](https://creativecommons.org/licenses/by-sa/4.0/)). By SE policy, any vandalism will be reverted."},
+
+{ "name": "[A] Vandalism", "description": "Please do not vandalize your posts. By posting on the Stack Exchange network, you've granted a non-revocable right for SE to distribute that content (under the [CC BY-SA 4.0 license](https://creativecommons.org/licenses/by-sa/4.0/)). By SE policy, any vandalism will be reverted."},
+
+]
+)
